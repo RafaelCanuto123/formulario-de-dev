4e3795c3a9c971d9f3b927bc8b59a9fcccbb8a9c
RafaelCanuto123 <41522952+RafaelCanuto123@users.noreply.github.com>
2021-12-30 13:38:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function cadastro(){
+    $('.cadastrar').on('click',function(){
+        $("#form").show();
+    });
+
+}
+function cancelar(){
+    $('.cancelar').on('click', function(){
+        $("#form").hide();
+    })
+}
